e122b504a1e495036d4d6b87afb1f2d03f59530d
Vinicius <vpess@protonmail.com>
2022-04-25 23:42:33 -0300

    Criação de json com status de erro, além do texto a ser exibido para o erro 400.

--- app/long_args.py
+++ app/long_args.py
@@ -1,3 +1,5 @@
+from app.response_models import Message
+
 class LongArgs:
 
     @staticmethod
@@ -14,8 +16,25 @@ class LongArgs:
                 "description": "Executa ações no Active Directory. **As ações não são realizadas em lote.**"
             }
         ]
+        
+    @staticmethod
+    def json_responses():
+        return {
+            404: {
+                "model": Message
+            },
+            400: {
+                "model": Message
+            },
+            422: {}
+        }
 
     @staticmethod
-    def json_not_found(object_str: str):
+    def info_not_found(object_str: str):
         return f"Não foi possível localizar '{object_str}' no Active Directory. Revise o que foi digitado, e se " \
                f"necessário, tente novamente."
+
+    @staticmethod
+    def info_bad_request(object_str: str, object_type: str):
+        return f"O objeto {object_str} foi encontrado no Active Directory, mas é do tipo {object_type.upper()}. " \
+               f"Utilize a chamada adequada."
