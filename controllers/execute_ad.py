534e2153eadda1b8650d5e40229a1783e4d361cc
Vinicius <vpess@protonmail.com>
2022-04-30 01:04:38 -0300

    Alteração de exceções de erro para a classe 'ErrorHandling'.

--- controllers/execute_ad.py
+++ controllers/execute_ad.py
@@ -1,6 +1,5 @@
 from pyad import pyad
-from app.ad_api import AdApi
-from app.long_args import LongArgs
+from app.error_handling import ErrorHandling
 from controllers.search_ad import SearchAD
 from models.active_directory import ActiveDirectory
 
@@ -17,33 +16,24 @@ class ExecuteAd:
         ad_group_type = SearchAD(self.group_str).get_type()
 
         if ad_obj_type is None:
-            response_detail = LongArgs.info_not_found(self.object_str)
-            AdApi.error_handling(404, response_detail)
-
+            ErrorHandling.raise_not_found(self.object_str)
         elif ad_obj_type == 'group':
-            response_detail = LongArgs.info_bad_request(self.object_str, ad_obj_type)
-            AdApi.error_handling(400, response_detail)
-
+            ErrorHandling.raise_bad_request(self.object_str, ad_obj_type)
         elif ad_group_type is None:
-            response_detail = LongArgs.info_not_found(self.group_str)
-            AdApi.error_handling(404, response_detail)
-
+            ErrorHandling.raise_not_found(self.group_str)
         elif ad_group_type != 'group':
-            response_detail = LongArgs.info_bad_request(self.group_str, ad_group_type)
-            AdApi.error_handling(400, response_detail)
-
+            ErrorHandling.raise_bad_request(self.group_str, ad_group_type)
         else:
             return True
 
     def run_action(self, action: str):
+        validation = self.validation()
         success_msg = {
             'add': 'adicionado ao grupo',
             'del': 'removido do grupo'
         }
 
-        validation = self.validation()
         if validation:
-
             actions = {
                 'add': self.object.add_to_group,
                 'del': self.object.remove_from_group
@@ -54,7 +44,6 @@ class ExecuteAd:
                 actions[action](self.group)
                 return {'message': f'{self.object_str} {success_msg[action]} {self.group_str}.'}
             except Exception as err:
-                response_detail = LongArgs.info_unprocessable_entity(f'{err}')
-                AdApi.error_handling(422, response_detail)
+                ErrorHandling.raise_unprocessable_entity(str(err))
         else:
             return validation
